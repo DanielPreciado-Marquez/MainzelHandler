2f6fad5add57dbe7b3c115d8f10fc0f6eb51d49e
Daniel P <daniel.preciado27@gmail.com>
2020-10-30 14:47:28 +0100

    Added site for depseudonymization

---
+++ demonstrator/demonstrator-app/src/main/resources/static/js/depseudonymization.js
@@ -0,0 +1,71 @@
+'use strict'
+
+var pseudonyms = [];
+var pseudonymizationService;
+
+window.onload = function () {
+    pseudonymizationService = new PseudonymizationService(contextPath);
+
+    updateList();
+
+    document.getElementById("add-pseudonym-button").addEventListener("click", addPseudonym);
+    document.getElementById("depseudonymize-button").addEventListener("click", updatePseudonyms);
+}
+
+function addPseudonym() {
+    const pseudonymForm = document.getElementById("pseudonym-form");
+    const pseudonym = pseudonymForm["pseudonym-input"].value;
+
+    pseudonyms.push(pseudonym);
+    updateList();
+}
+
+async function updatePseudonyms() {
+    document.getElementById("server-error").innerHTML = "";
+
+    try {
+        const { depseudonymized, invalid } = await pseudonymizationService.depseudonymize(pseudonyms);
+        updateList(depseudonymized, invalid);
+    } catch (error) {
+        document.getElementById("server-error").innerHTML = error.message;
+    }
+}
+
+function updateList(depseudonymized, invalid) {
+    depseudonymized = depseudonymized ?? new Map();
+    invalid = invalid ?? [];
+
+    const pseudonymList = document.getElementById("pseudonym-list");
+
+    pseudonymList.innerHTML = "";
+
+    for (const [key, pseudonym] of pseudonyms.entries()) {
+        const listElement = document.createElement('li');
+
+        if (depseudonymized.has(pseudonym)) {
+            listElement.appendChild(document.createTextNode("key: " + key + ", pseudonym: " + pseudonym + ", IDAT: " + JSON.stringify(depseudonymized.get(pseudonym))));
+        } else if (invalid.includes(pseudonym)) {
+            listElement.appendChild(document.createTextNode("key: " + key + ", pseudonym: " + pseudonym + ", Nicht gefunden"));
+        } else {
+            listElement.appendChild(document.createTextNode("key: " + key + ", pseudonym: " + pseudonym + ", Nicht angefragt"));
+        }
+
+        addDeleteButton(key, listElement);
+        pseudonymList.appendChild(listElement);
+    }
+
+    if (pseudonymList.innerHTML === "") {
+        pseudonymList.innerHTML = "Keine Pseudonyme";
+    }
+}
+
+function addDeleteButton(key, listElement) {
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Löschen";
+
+    deleteButton.addEventListener("click", () => {
+        pseudonyms.splice(pseudonyms.indexOf(key), 1);
+        updateList();
+    });
+    listElement.appendChild(deleteButton);
+}
